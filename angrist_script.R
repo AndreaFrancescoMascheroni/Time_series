c684eb3e84014b8de0118c09dacdc8ae3aa6a8b0
Andrea <andremasche93@gmail.com>
2021-04-07 08:47:56 +0200

    qualche prova su script R

--- angrist_script.R
+++ angrist_script.R
@@ -1,4 +1,6 @@
 library(haven)
+library(RStata)
+chooseStataBin()
 #read.table("/Users/mascheroni/Documents/Angrist1990/cwhsb.dta", header=TRUE, skip=3,sep="\");
 Dataset1 <- read_dta("/Users/mascheroni/Documents/Angrist1990/cwhsb.dta")
 View(Dataset1)
@@ -19,3 +21,13 @@ Dataset6 <- read_dta("/Users/mascheroni/Documents/Angrist1990/sipp2.dta")
 View(Dataset6)
 
 #Draft_Lottery_Numbers.xls
+install.packages('RStata')
+
+if (!require(RStata)) install.packages("RStata"); 
+library(RStata) # this will install RStata if not already installed
+chooseStataBin()
+
+#stata("Angrist1990_Fig3.do", 
+#      stata.path = "/var/folders/f5/r5r171jd2g77lyj9vkrrql600000gq/T//RtmpuXBTQy/downloaded_packages/RStata", # yours probably differs: use the chooseStataBin() command on windows or linux machines; on Macs, right click on the Stata app, select "Show Package Contents", then see what's in the Contents/MacOS/ directory
+#      stata.version = 13)  # again, specify what _you_ have
+stata("/Users/mascheroni/Documents/master/secondo/Time_series/dataverse_files/Angrist1990_Fig3.do")
